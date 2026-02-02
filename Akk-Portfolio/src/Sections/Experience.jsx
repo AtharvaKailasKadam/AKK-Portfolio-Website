90807dbc00a1b50c7ad8317436ec4dea09189eb3
Your Name <atharvakailaskadam1809@gmail.com>
2026-02-03 00:17:34 +0530

    Done

--- Akk-Portfolio/src/Sections/Experience.jsx
+++ Akk-Portfolio/src/Sections/Experience.jsx
@@ -1,11 +1,163 @@
-import React from "react";
+    import React, { useMemo } from "react";
+    import { experiences } from "./Experiences";
+    import { useTransform, motion } from "framer-motion";
+    import { useScroll } from "framer-motion";
+    import { useState, useEffect, useRef } from "react";
 
+
+function ExperienceItem({ idx, start, end, role, company, duration, description, layout, scrollYProgress}) {
+    const scale = useTransform(scrollYProgress, [start , end], [0, 1]);
+    const opacity = useTransform(scrollYProgress, [start , end], [0, 1], {clamp: true});
+    const y = useTransform(scrollYProgress, [start, end], [idx % 2===0 ? 30 : -30, 0]);
+    const x = useTransform(scrollYProgress, [start, end], [-24, 0]);
+
+    if(layout === "desktop")
+    {
+        return(
+            <div className = " relative flex flex-1 justify-center items-center min-w-0">
+                <motion.div className="z-10 w-7 h-7 rounded-full bg-white shadow-[0_0_0_8px_rgba(255,255,255,0.1)]"
+                    style={{scale, opacity}}>
+                </motion.div>
+                <motion.div className={`absolute ${idx % 2 === 0 ? "-top-8" :"-bottom-8"} w-[3px] bg-white/40`}
+                style={{height: 40, opacity}}>
+                </motion.div>
+
+                <motion.article className = {`absolute ${idx % 2 === 0 ? "bottom-12" :"top-12"} bg-gray-900/80 backdrop-blur border border-gray-700/70 rounded-xl p-7 w-[320px] shadow-lg `}
+                style={{opacity, y, maxWidth: "90vw"}}
+                transition={{duration : 0.4, delay : idx*0.15}}>
+                    <h3 className="text-xl font-semibold text-white">
+                        {role}
+                    </h3>
+                    <p>
+                        {company} | {duration}
+                    </p>
+                    <ul className="text-md text-gray-300 break-words">
+                        {description.map((item, index) => (
+                            <li key={index} className="text-md text-gray-300 break-words">
+                                {item}
+                            </li>
+                        ))}
+                    </ul>
+                </motion.article>
+            </div>
+        )
+}
+    return (
+        <div className="relative flex items-start">
+            <motion.div className="absolute -left-[14px] top-3 z-10 w-7 h-7 rounded-full bg-white shadow-[0_0_0_8px_rgba(255, 255, 255, 0.1)]"
+            style={{scale, opacity}}>
+            </motion.div>
+            <motion.article className="bg-gray-900/80 backdrop-blur border border-gray-700/70 rounded-xl p-5 w-[90vw] max-w-sm ml-6 shadow-lg"
+            style={{opacity, x}}
+            transition={{duration : 0.4, delay : idx*0.15}}>
+                <h3 className="text-xl font-semibold text-white break-words">
+                    {role}
+                </h3>
+                <p>
+                    {company} | {duration}
+                </p>
+                <ul className="text-md text-gray-300 break-words">
+                    {description.map((item, index) => (
+                        <li key={index} className="text-md text-gray-300 break-words">
+                            {item}
+                        </li>
+                    ))}
+                </ul>
+            </motion.article>
+        </div>
+    )
+}
 export const Experience = () => {
+
+    const sceneRef = useRef(null);
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const checkMobile = () => setIsMobile(window.innerWidth < 760);
+        checkMobile();
+        window.addEventListener("resize", checkMobile, { passive: true });
+        return() => window.removeEventListener("resize", checkMobile)
+    },[])
+
+    const SCENE_HEIGHT_VH = isMobile ? 160*experiences.length : 120*experiences.length;
+    const { scrollYProgress } = useScroll({
+        target: sceneRef,
+        offset:["start start", "end end"],
+    })
+
+    const thresholds = useMemo(() => experiences.map((_,i) => (i+1) / experiences.length),[]);
+
+    const lineSize = useTransform(scrollYProgress, (v) => `${v*100}%`)
+
     return (
         <>
-            <div className='w-full h-screen'>
-                Experience
-            </div>
+            <section id="experience" className="relative bg-black text-white">
+                <div ref={sceneRef}
+                style={{height: `${SCENE_HEIGHT_VH}vh`, minHeight:"120vh"}}
+                className="relative">
+                    <div className="sticky top-0 h-screen flex flex-col">
+                        <motion.h2 className="items-center text-center overflow-hidden mt-10 sm:mt-20 text-4xl sm:text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#1cd8d2] via-[#00bf8f] to-[#1cd8d2] z-10"
+                            initial={{opacity:0, y:30}}
+                            animate={{opacity:1, y:0}}
+                            transition={{duration:0.6}}
+                            whileInView={{opacity:1, y:0}}
+                            viewport={{once:true, amount:0.6}} >
+                            <u>EXPERIENCE</u>
+                        </motion.h2>
+                        <div className="flex flex-1 items-center justify-center px-6 pb-10">
+                            {!isMobile && (
+                                <div className="relative w-full max-w-7xl">
+                                    <div className="relative h-[6px] bg-white/15 rounded">
+                                        <motion.div className="absolute left-0 top-0 h-[6px] bg-white origin-left"
+                                        style={{width : lineSize}}>
+                                        </motion.div>
+                                    </div>
+
+                                    <div className="relative flex justify-between mt-0">
+                                        {experiences.map((exp, idx) =>
+                                        <ExperienceItem
+                                        key={idx}
+                                        idx={idx}
+                                        start={idx === 0 ? 0 : thresholds[idx-1]}
+                                        end={thresholds[idx]}
+                                        scrollYProgress={scrollYProgress}
+                                        layout="desktop"
+                                        {...exp}
+                                        />
+                                        )}
+                                    </div>
+
+                                </div>
+                            )}
+
+                            {isMobile && (
+                                <div className="relative w-full max-w-md">
+                                    <div className="relative left-0 top-0 h-[6px] bg-white/15 rounded">
+                                        <motion.div className="absolute left-0 top-0 h-[6px] bg-white origin-left"
+                                        style={{width : lineSize}}>
+                                        </motion.div>
+                                    </div>
+
+                                    <div className="relative flex flex-col gap-10 ml-10 justify-between mt-6">
+                                        {experiences.map((exp, idx) =>
+                                        <ExperienceItem
+                                        key={idx}
+                                        idx={idx}
+                                        start={idx === 0 ? 0 : thresholds[idx-1]}
+                                        end={thresholds[idx]}
+                                        scrollYProgress={scrollYProgress}
+                                        layout="mobile"
+                                        {...exp}
+                                        />
+                                        )}
+                                    </div>
+                                </div>
+                            )
+                        }
+                        </div>
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
---
+++ Akk-Portfolio/src/Sections/Experiences.jsx
@@ -0,0 +1,44 @@
+    export const experiences = [
+    {
+        role: "Frontend Developer (Fresher)",
+        company: "Self-Driven Projects",
+        duration: "2024 – Present",
+        type: "Project-Based Experience",
+        description: [
+        "Built responsive and interactive web applications using React.js",
+        "Developed reusable UI components with Tailwind CSS",
+        "Implemented smooth animations using Framer Motion",
+        "Worked with modern JavaScript (ES6+) and component-based architecture and Deployed on Vercel and Netlify",
+        ],
+        techStack: [
+        "React",
+        "JavaScript",
+        "Tailwind CSS",
+        "Framer Motion",
+        "HTML",
+        "CSS",
+        "Git",
+        "GitHub",
+        ],
+    },
+    {
+        role: "MERN Stack Developer (Learning & Projects)",
+        company: "Personal Projects",
+        duration: "2025 – Present",
+        type: "Hands-on Learning",
+        description: [
+        "Built full-stack applications using MongoDB, Express, React, and Node.js",
+        "Designed REST APIs and handled frontend-backend integration",
+        "Managed application state and user interactions",
+        "Focused on clean code structure and scalability",
+        ],
+        techStack: [
+        "MongoDB",
+        "Express",
+        "React",
+        "Node.js",
+        "JavaScript",
+        "Git",
+        ],
+    },
+    ];
